ae776bdd3095cebeea9f53d057428faa74e2f8b6
samantavein <samantavein@gmail.com>
2023-10-27 20:47:09 +0300

    Your commit message

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    const menuLinks = document.querySelector('.menu-links');
-    menuLinks.style.display = (menuLinks.style.display === 'block') ? 'none' : 'block';
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu-icon').addEventListener('click', function() {
+      const menuLinks = document.querySelector('.menu-links');
+      menuLinks.style.display = (menuLinks.style.display === 'block') ? 'none' : 'block';
+    });
+  });
+  
